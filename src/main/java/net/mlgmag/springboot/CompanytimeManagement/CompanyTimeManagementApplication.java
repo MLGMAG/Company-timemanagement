4c5d91b18d16b4a80e09ed8b6aec723863aeddee
Magomed-PC <akhmedovmagomed2001@gmail.com>
2018-06-10 23:15:18 +0300

    Spring-boot init

---
+++ src/main/java/net/mlgmag/springboot/CompanytimeManagement/CompanyTimeManagementApplication.java
@@ -0,0 +1,12 @@
+package net.mlgmag.springboot.CompanytimeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyTimeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyTimeManagementApplication.class, args);
+	}
+}
---
+++ src/test/java/net/mlgmag/springboot/CompanytimeManagement/CompanyTimeManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package net.mlgmag.springboot.CompanytimeManagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompanyTimeManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
